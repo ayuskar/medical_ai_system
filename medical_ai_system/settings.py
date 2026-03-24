85cb3d7c335b0ca24278115475e16ec41e1d931b
Aayush Karki <144099541+ayuskar@users.noreply.github.com>
2026-03-24 13:16:40 +0545

    Update settings.py

--- medical_ai_system/settings.py
+++ medical_ai_system/settings.py
@@ -172,10 +172,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aayushkrki11@gmail.com'
-EMAIL_HOST_PASSWORD = 'cfoi wnid xkvk zurd'  # Google App Password
-DEFAULT_FROM_EMAIL = 'aayushkrki11@gmail.com'
-SERVER_EMAIL = 'aayushkrki11@gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''  # Google App Password
+DEFAULT_FROM_EMAIL = ''
+SERVER_EMAIL = ''
 
 # Admin email for notifications
-ADMIN_EMAIL = 'aayushkrki11@gmail.com'
+ADMIN_EMAIL = ''
